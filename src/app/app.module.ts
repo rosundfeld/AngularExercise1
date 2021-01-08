a3488286ca0c7fd252e71d85a24a46117b454459
Rodrigo Sundfeld <rodrigo.psundfeld@gmail.com>
2021-01-08 07:48:20 -0300

    All changes so far

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TopoComponent } from './topo/topo.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopoComponent,
+    RodapeComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
